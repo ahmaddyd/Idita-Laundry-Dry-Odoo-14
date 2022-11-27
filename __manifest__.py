171a131a6c78c68608dee678aeea085d68e02f60
ahmaddyd <ahmdyulian.86@gmail.com>
2022-11-27 15:17:08 +0700

    Modifier manifest.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "idita_laundry",
+    'name': "IDITA LAUNDRY",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Sistem Aplikasi ERP pencucian baju idita""",
 
     'description': """
-        Long description of module's purpose
+        Sistem Aplikasi ERP pencucian baju idita
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Ahmad Yulian Dinata",
+    'website': "https://www.linkedin.com/in/ahmaddyd/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
+    'application': True,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
